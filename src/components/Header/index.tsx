6abe5b8ecbc84efdc4c7488945cec528bdca78f1
Luiz Humphries <luizgustavo.frota@outlook.com>
2022-11-17 17:55:10 -0300

    Menu config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
+import { ActiveLink } from '../ActiveLink';
+
 export function Header() {
+    
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/' legacyBehavior>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts' legacyBehavior>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
-
                 <SignInButton />
             </div>
         </header>
