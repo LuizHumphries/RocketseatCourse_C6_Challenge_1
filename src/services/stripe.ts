43adc5140563ffe828c82772d1ff78bf92ef255c
Luiz Humphries <luizgustavo.frota@outlook.com>
2022-12-23 20:57:54 -0300

    end

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,14 +1,10 @@
 import Stripe from "stripe";
-import packageInfo from '../../package.json'
+import packageInfo from "../../package.json";
 
-export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY,
-    {
-        apiVersion: "2020-08-27",
-        appInfo: {
-            name: "Ignews",
-            version: packageInfo.version           
-        },
-    } 
-
-);
+export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+  apiVersion: "2022-08-01",
+  appInfo: {
+    name: "Ignews",
+    version: packageInfo.version,
+  },
+});
